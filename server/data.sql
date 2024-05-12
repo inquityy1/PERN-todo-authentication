1bf2d4a36e8b1e7d3003a6967d8c44a058b3aca5
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-05-12 23:18:18 +0200

    setting up backend

---
+++ server/data.sql
@@ -0,0 +1,29 @@
+-- CREATE DATABASE todoapp;
+
+-- CREATE TABLE todos (
+--     id VARCHAR(255) PRIMARY KEY,
+--     user_email VARCHAR(255),
+--     title VARCHAR(30),
+--     progress INT,
+--     date VARCHAR(300)
+-- );
+
+-- CREATE TABLE users (
+--     email VARCHAR(255) PRIMARY KEY,
+--     hashed_password VARCHAR(255)
+-- );
+
+-- INSERT INTO todos(
+--     id,
+--     user_email,
+--     title,
+--     progress,
+--     date
+-- )
+-- VALUES (
+--     '0',
+--     'nebojsa@test.com',
+--     'First todo',
+--     10,
+--     'Thu Dec 29 2022 13:25:45 GMT+0400 (Gulf Standard Time)'
+-- );
