0a123e61ec27f7d57e3206a77f4bbe71858eb404
inquityy1 <nebojsanikolic12350@yahoo.com>
2024-05-13 16:48:24 +0200

    entire auth todo app is done

--- client/src/App.js
+++ client/src/App.js
@@ -39,11 +39,13 @@ function App() {
       {authToken && (
         <>
           <ListHeader listName={"Holiday Tick list"} getData={getData} />
+          <p className="user-email">Welcome back {userEmail}</p>
           {sortedTasks?.map((task) => (
             <ListItem key={task.id} task={task} getData={getData} />
           ))}
         </>
       )}
+      <p className="copyright">© Nebojsa Nikolic</p>
     </div>
   );
 }
--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import Modal from "./Modal";
+import { useCookies } from "react-cookie";
 
 function ListHeader({ listName, getData }) {
+  const [cookies, setCookie, removeCookie] = useCookies(null);
   const [showModal, setShowModal] = useState(false);
 
   const signOut = () => {
     console.log("signout");
+    removeCookie("Email");
+    removeCookie("AuthToken");
+    window.location.reload();
   };
 
   return (
